284be6d33883cdea6ea62ec3c2f7effb9a438623
quizmania101 <batata0509@naver.com>
2022-02-12 22:24:30 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,33 +1,33 @@
+const url = 'https://elated-knuth-b773ed.netlify.app/';
+
 function setShare(){
-    var resultImg = document.querySelector('#resultImg');
-    var resultAlt = resultImg.firstElementChild.alt;
-    const shareTitle = '2022 빵덕후 능력 고사 결과';
-    const shareDes = infoList[resultAlt].name;
-    const shareImage = url + 'img/image-' + resultAlt + '.png';
-    const shareURL = url + 'page/result-' + resultAlt + '.html';
-}
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '2022 빵덕후 능력 고사 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
 
-function kakaoShare(){
- Kakao.Link.sendDefault({
-  objectType: 'feed',
-  content: {
-    title: shareTitle,
-    description: shareDes,
-    imageUrl: shareImage,
-    link: {
-      mobileWebUrl: shareURL,
-      webUrl: shareURL,
-    },
-  },
-
-  buttons: [
-    {
-      title: '결과확인하기',
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
       link: {
         mobileWebUrl: shareURL,
-        webUrl: shareURL,
+        webUrl: shareURL
       },
     },
-  ]
-});
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
 }
