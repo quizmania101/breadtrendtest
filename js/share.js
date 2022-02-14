6fcd783fe26813afd4d137964d0e2f7da90352b8
quizmania101 <batata0509@naver.com>
2022-02-14 15:59:12 +0900

    공유이미지

--- js/share.js
+++ js/share.js
@@ -5,7 +5,7 @@ function setShare(){
   var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = '나의 2022 빵덕후 능력 고사 결과는?';
   const shareDes = infoList[10-resultAlt].name;
-  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareImage = url + 'img/share/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
 
   Kakao.Link.sendDefault({
